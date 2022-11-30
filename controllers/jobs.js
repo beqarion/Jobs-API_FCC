96c5e839cfc0973981426ad0719e3e44fc285667
beqarion <beqarioni@yahoo.co.uk>
2022-11-30 18:13:21 +0400

    work->home

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,12 +1,23 @@
+const Job = require("../models/Job")
+const { StatusCodes } = require("http-status-codes")
+const { BadRequestError, NotFoundError } = require("../errors")
+
 const getAllJobs = async (req, res, next) => {
-  res.send("get all jobs")
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt")
+  console.log(jobs)
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
+
 const getJob = async (req, res, next) => {
   res.send("get a single job")
 }
+
 const createJob = async (req, res, next) => {
-  res.json(req.user)
+  req.body.createdBy = req.user.userId
+  const job = await Job.create(req.body)
+  res.status(StatusCodes.CREATED).json(job)
 }
+
 const updateJob = async (req, res, next) => {
   res.send("update job")
 }
