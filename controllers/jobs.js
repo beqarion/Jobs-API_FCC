460b443e02f5680b6d6262e495ba1bcb0abaa420
Bekarion <beqarioni@yahoo.co.uk>
2022-12-03 01:02:00 +0400

    3 dec friday evening

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -49,7 +49,16 @@ const updateJob = async (req, res, next) => {
 }
 
 const deleteJob = async (req, res, next) => {
-  res.send("delete job")
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req
+
+  const job = await Job.findByIdAndRemove({ _id: jobId, createdBy: userId })
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).send()
 }
 
 module.exports = {
