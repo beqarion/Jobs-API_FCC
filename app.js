cbe8ec9c0d7a87c54e8d86ad57f611fdf64b6219
Bekarion <beqarioni@yahoo.co.uk>
2022-12-10 00:41:31 +0400

    commited pre heroku

--- app.js
+++ app.js
@@ -33,7 +33,11 @@ app.use(express.json())
 app.use(helmet)
 app.use(cors)
 app.use(xss)
+app.use(rateLimiter)
 
+app.get("/", (req, res, next) => {
+  res.send("jobs api")
+})
 // routes
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/jobs", authenticateUser, jobsRouter)
